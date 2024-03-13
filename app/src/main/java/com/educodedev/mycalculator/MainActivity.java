dff5cfe2f4b77678f5ed0455a73dd6f9ddd64510
eduardoandr3s <edu92seg@gmail.com>
2024-03-13 18:19:53 +0100

    calculadora jona

--- app/src/main/java/com/educodedev/mycalculator/MainActivity.java
+++ app/src/main/java/com/educodedev/mycalculator/MainActivity.java
@@ -2,6 +2,7 @@ package com.educodedev.mycalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,132 +10,149 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button btn_suma;
-    private Button btn_resta;
-    private Button btn_multiplicacion;
-    private Button btn_division;
-    private Button btn_limpiar;
-    private Button btn_factorial;
+    double firstNum;
+    String operation;
 
-
-    private TextView text_resultado;
-    private EditText edit_numero_uno;
-    private EditText edit_numero_dos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn_limpiar = findViewById(R.id.btn_limpiar);
-        btn_limpiar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                limpiar();
-            }
+        Button num0 = findViewById(R.id.num0);
+        Button num1 = findViewById(R.id.num1);
+        Button num2 = findViewById(R.id.num2);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button num3 = findViewById(R.id.num3);
+        Button num4 = findViewById(R.id.num4);
+        Button num5 = findViewById(R.id.num5);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button num6 = findViewById(R.id.num6);
+        Button num7 = findViewById(R.id.num7);
+        Button num8 = findViewById(R.id.num8);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) Button num9 = findViewById(R.id.num9);
+
+        Button percent = findViewById(R.id.percent);
+        Button plusMinus = findViewById(R.id.plusMinus);
+        Button ac = findViewById(R.id.ac);
+        Button div = findViewById(R.id.div);
+        Button times = findViewById(R.id.times);
+        Button min = findViewById(R.id.min);
+        Button plus = findViewById(R.id.plus);
+        Button equal = findViewById(R.id.equal);
+        Button point = findViewById(R.id.point);
+
+        TextView screen = findViewById(R.id.screen);
+
+
+        ac.setOnClickListener(view -> {
+            firstNum = 0;
+            screen.setText("0");
         });
 
 
-        btn_factorial = findViewById(R.id.btn_factorial);
-        btn_factorial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_resultado.setText(factorial
-                        (Double.parseDouble(edit_numero_uno.getText().toString())) + "");
-            }
-        });
+        ArrayList<Button> nums = new ArrayList<>();
+        nums.add(num0);
+        nums.add(num1);
+        nums.add(num2);
+        nums.add(num3);
+        nums.add(num4);
+        nums.add(num5);
+        nums.add(num6);
+        nums.add(num7);
+        nums.add(num8);
+        nums.add(num9);
 
-        btn_suma = findViewById(R.id.button_suma);
-        btn_suma.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_resultado.setText(suma
-                        (Double.parseDouble(edit_numero_uno.getText().toString()),
-                                Double.parseDouble(edit_numero_dos.getText().toString())) + "");
-            }
-        });
-
-        btn_resta = findViewById(R.id.button_resta);
-        btn_resta.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_resultado.setText(resta
-                        (Double.parseDouble(edit_numero_uno.getText().toString()),
-                                Double.parseDouble(edit_numero_dos.getText().toString())) + "");
-            }
-        });
-
-        btn_multiplicacion = findViewById(R.id.button_multiplicacion);
-        btn_multiplicacion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_resultado.setText(multiplicacion
-                        (Double.parseDouble(edit_numero_uno.getText().toString()),
-                                Integer.parseInt(edit_numero_dos.getText().toString())) + "");
-            }
-        });
-
-        btn_division = findViewById(R.id.button_division);
-        btn_division.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                text_resultado.setText(division
-                        (Double.parseDouble(edit_numero_uno.getText().toString()),
-                                Integer.parseInt(edit_numero_dos.getText().toString())) + "");
-            }
-        });
-
-        text_resultado = findViewById(R.id.resultado);
-
-        edit_numero_uno = findViewById(R.id.numero1);
-        edit_numero_dos = findViewById(R.id.numero2);
-
-    }
-
-    public void limpiar() {
-        edit_numero_uno.setText("");
-        edit_numero_dos.setText("");
-        Toast.makeText(getApplicationContext(), "Comienza de nuevo"
-                , Toast.LENGTH_LONG).show();
-    }
-
-    public double suma(double a, double b) {
-        return a + b;
-    }
-
-    public double resta(double a, double b) {
-        return a - b;
-    }
-
-    public double multiplicacion(double a, double b) {
-        return a * b;
-    }
-
-    public double factorial(double a) {
-        edit_numero_dos.setText("");
-        double fact = a;
-        for (int i = 1; i < a; i++) {
-           fact = fact *(i);
+        for (Button b : nums) {
+            b.setOnClickListener(View -> {
+                if (!screen.getText().toString().equals("0")) {
+                    screen.setText(screen.getText().toString() + b.getText().toString());
+                } else {
+                    screen.setText(b.getText().toString());
+                }
+            });
         }
-      
-            Toast.makeText(getApplicationContext(), "Lo cálcula en base al número en el campo de arriba"
-                    , Toast.LENGTH_LONG).show();
+        ArrayList<Button> operators = new ArrayList<>();
+        operators.add(div);
+        operators.add(times);
+        operators.add(plus);
+        operators.add(min);
 
-
-        return fact;
-    }
-
-    public double division(double a, double b) {
-        double respuesta = 0;
-
-        if (b != 0) {
-            respuesta = a / b;
-        } else {
-            Toast.makeText(getApplicationContext(), "No se pueden dividir números por CERO ¿Eres tonto?!!"
-                    , Toast.LENGTH_LONG).show();
+        for (Button b : operators) {
+            b.setOnClickListener(view -> {
+                firstNum = Double.parseDouble(screen.getText().toString());
+                operation = b.getText().toString();
+                screen.setText("0");
+            });
         }
-        return respuesta;
+
+
+        point.setOnClickListener(view -> {
+            if (!screen.getText().toString().contains(".")) {
+                screen.setText(screen.getText().toString() + ".");
+            }
+        });
+
+
+        plusMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String currentText = screen.getText().toString();
+
+                if (currentText.startsWith("-")) {
+                    screen.setText(currentText.substring(1));
+                } else {
+                    screen.setText("-" + currentText);
+                }
+            }
+        });
+
+        percent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentText = screen.getText().toString();
+
+                double currentNum = Double.parseDouble(currentText);
+
+                double percent = firstNum * (currentNum / 100);
+
+                screen.setText(String.valueOf(percent));
+            }
+        });
+
+
+        equal.setOnClickListener(view -> {
+            double secondNum = Double.parseDouble(screen.getText().toString());
+            double resultado;
+            switch (operation) {
+                case "÷":
+                    if (secondNum == 0) {
+                        resultado = 0;
+                        break;
+                    } else {
+                        resultado = firstNum / secondNum;
+                    }
+                    break;
+                case "X":
+                    resultado = firstNum * secondNum;
+                    break;
+                case "+":
+                    resultado = firstNum + secondNum;
+                    break;
+                case "-":
+                    resultado = firstNum - secondNum;
+                    break;
+                default:
+                    resultado = firstNum + secondNum;
+                    break;
+            }
+            screen.setText(String.valueOf(resultado));
+            firstNum = resultado;
+        });
+
     }
+
 }
